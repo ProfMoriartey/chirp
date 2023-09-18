22881366fdbf9ba2265a34a288187e21a126e6d2
profmoriartey <kaito9814@gmail.com>
2023-09-18 12:38:51 +0300

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -21,7 +21,9 @@ export default function Home() {
           {!user.isSignedIn && <SignIn />}
           {!!user.isSignedIn && <SignOutButton />}
         </div>
-        
+        <div>
+          {data?.map((post) => <div key={post.id}>{post.content}</div>)}
+        </div>
       </main>
     </>
   );
